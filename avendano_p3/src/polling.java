3921b93b8eaacf839ca054c026b1a9340e10c8b6
aavendano23 <ariane.avendano@knights.ucf.edu>
2019-09-20 03:04:07 -0400

    update p3

--- avendano_p3/src/polling.java
+++ avendano_p3/src/polling.java
@@ -3,9 +3,36 @@ import java.util.Scanner;
 public class polling {
 
 	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
 		int[][] poll = new int[5][10]; 
-		int[] topics = new int[5]; 
-		int i;
+		String[] topics = {"Gender Equality", "Quality Education", "Zero Hunger", "Clean Water", "Climate Action"}; 
+		int i, userRate;
+		final int NUM_TOPICS = 5;
+		int counter = 0;
+		
+		System.out.println("Please rate the 5 topics 1 through 10. 1 being the least important and 10 being the most important");
+		
+		while(true) {
+			
+			if (counter == 3) {
+				break;
+			}
+			
+			for (i = 0; i < NUM_TOPICS; i++) {
+				System.out.printf("Please rate the %s 1 through 10. 1 being the least important and 10 being the most important\n", topics[i] );
+				userRate = scnr.nextInt();
+				while (userRate > 10) {
+					System.out.println("Your rating is out of bounds. Please enter a rating between 1 - 10.");
+					userRate = scnr.nextInt();
+				}
+				poll[i][userRate - 1]++;
+			}
+			
+			System.out.println();
+			
+			counter++;			
+		}
+		
 		
 		// display table 
 		System.out.printf("%-15s%s\n", "Topics", "Ratings"); 
